68c9914ec1c1e04886d1fe4259f035f0c088639b
MAXIM CHERNY <bm.black.max@gmail.com>
2015-05-08 00:40:41 +0300

    Почти доделанный конструктор материалов

--- Assets/Editor/ItemsDataBaseEditor.cs
+++ Assets/Editor/ItemsDataBaseEditor.cs
@@ -9,8 +9,18 @@ public class ItemsDataBaseEditor : Editor {
 	test tt;
 	public override void OnInspectorGUI () {
 		ItemsDataBaseScript myScript = (ItemsDataBaseScript)target;
+
 		EditorGUILayout.HelpBox("New Item", MessageType.None);
 		myScript.itemType = (ItemsDataBaseScript.typeOfItem)EditorGUILayout.EnumPopup("Item type", myScript.itemType);
+		myScript.itemName = EditorGUILayout.TextField("Item name", myScript.itemName);
+		myScript.icon = (Texture2D) EditorGUILayout.ObjectField("Image", myScript.icon, typeof (Texture2D), false);
+
+		if (myScript.itemType == 0) {
+			myScript.materialType = (ItemsDataBaseScript.allMaterials)EditorGUILayout.EnumPopup("Material type", myScript.materialType);
+		}
+		else {
+			myScript.itemName = EditorGUILayout.TextField("cheto", myScript.itemName);
+		}
 
 
 
--- Assets/ItemsDataBaseScript.cs
+++ Assets/ItemsDataBaseScript.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class ItemsDataBaseScript : MonoBehaviour {
 
-	public enum typeOfItem {Materials, Equpment};
-	public enum allTypes {Metal, Organic, Crystal};
+	public enum typeOfItem {Materials = 0, Equipment = 1};
+	public enum allMaterials {Metal, Organic, Crystal};
 
 	public typeOfItem itemType;
 	public Texture2D icon;
 	public string itemName;
-	public allTypes type;
+	public allMaterials materialType;
 	// Use this for initialization
 	void Start () {
 	
