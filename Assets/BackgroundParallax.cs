c95e6477e14de577bcb18db89a57ba96e0c40506
Alex <Kyvaevy@gmail.com>
2015-05-10 01:20:03 +0300

    optimised parallax

--- Assets/BackgroundParallax.cs
+++ Assets/BackgroundParallax.cs
@@ -5,9 +5,14 @@ public class BackgroundParallax : MonoBehaviour {
 	public Transform[] backgrounds; //backgrounds for changes with paralax
 	private float[] parallaxScales; //coff to paralax
 	public float smoothing = 1f; //coff of blur
-	
 	private Transform cam;
 	private Vector3 previousCamPos;
+
+	float parallax;
+	float backgroundTargetPosX;
+	float backgroundTargetPosY;
+	Vector3 backgroundTargetPos;
+
 	//вызывается прежде чем вызовется Start() ахуенно для настройки
 	void  Awake(){
 		cam = Camera.main.transform;
@@ -19,20 +24,17 @@ public class BackgroundParallax : MonoBehaviour {
 		parallaxScales = new float[backgrounds.Length];
 		
 		for (int i = 0; i<backgrounds.Length; i++){
-			parallaxScales[i] = backgrounds[i].position.z*-1;
+			parallaxScales[(backgrounds.Length-1)-i] = backgrounds[i].position.z*-1;
 		}
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float parallax;
-		float backgroundTargetPosX;
-		Vector3 backgroundTargetPos;
 		for (int i = 0; i<backgrounds.Length; i++){
 			parallax = (previousCamPos.x - cam.position.x)*parallaxScales[i];
 			backgroundTargetPosX = backgrounds[i].position.x+parallax;
-			backgroundTargetPos = new Vector3 (backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+			backgroundTargetPosY = backgrounds[i].position.y+(previousCamPos.y - cam.position.y)*(parallaxScales[(backgrounds.Length-1)-i]);//parallax*0.5f;
+			backgroundTargetPos = new Vector3 (backgroundTargetPosX, backgroundTargetPosY, backgrounds[i].position.z);
 			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing + Time.deltaTime);
 		};
 		previousCamPos = cam.position;
