3253342eed9abde4669a38b585c4776de75e9e1d
Alex <Kyvaevy@gmail.com>
2015-05-13 23:05:09 +0300

    test effect desert

---
+++ Assets/DeserEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeserEffect : MonoBehaviour {
+	public Transform[] DesertEffect;
+	// Use this for initialization
+	void Start () {
+		Transform cam = Camera.main.transform;
+		Debug.Log (DesertEffect[0].transform.position);
+		Debug.Log (DesertEffect.Length);
+		float x = 5.5f;
+		DesertEffect[0].position = new Vector3 (5.5f,5.5f,0);
+		Debug.Log (DesertEffect[0].transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
